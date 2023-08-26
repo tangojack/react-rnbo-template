b72cec32304458aa7e11d948396ae3f3243f8660
Tango Jack <tangojack@Tangos-Mac-mini.local>
2023-08-27 01:25:09 +0800

    Removed config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,27 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: path.join(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx|js|json)$/,
-                exclude: path.join(__dirname, 'node_modules'),
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
-                    }
-                },
-                resolve: {
-                    extensions: ['.ts', '.tsx', '.js', '.json'],
-                },
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.ejs'),
-        }),
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx|json)$/,
+        exclude: path.join(__dirname, "node_modules"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "index.ejs"),
+    }),
+  ],
+};
