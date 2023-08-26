6dda263fad9c19d800ec22d09ddb8c61cd808060
Tango Jack <tangojack@Tangos-Mac-mini.local>
2023-08-26 20:57:39 +0800

    Changes to gitignore

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
+app.use(express.static('public'))
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
